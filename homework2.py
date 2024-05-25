211101faedb652e6a9e6e902e9365f70e274a3ed
vladneki <vmedvet0@gmail.com>
2024-05-25 22:14:35 +0300

    first commit

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_dz = 12
+count_time = 1.5
+name_course = "Python"
+average_execution_time = (count_time / count_dz)
+print('Курс:', name_course,'всего задач:',count_dz, 'затрачено часов:', count_time, 'среднее время выполнения', average_execution_time, 'часа')
