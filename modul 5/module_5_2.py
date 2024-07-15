74b4fc0e1c1015fa7c301d0dd9eafd7a4c9daaa8
vladneki <vmedvet0@gmail.com>
2024-07-15 14:33:19 +0300

    dd

---
+++ modul 5/module_5_2.py
@@ -0,0 +1,24 @@
+class House:
+    def __init__(self,name,number_of_floors):
+        
+        self.name = name
+        self.number_of_floors = number_of_floors
+    
+    def __len__(self):
+        return self.number_of_floors
+    
+    def __str__(self):
+        return f"Название: {self.name}, кол - во этажей {self.number_of_floors}"
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
