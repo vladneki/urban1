6823b107cf3d0d0125f853561fca2de32206116f
vladneki <vmedvet0@gmail.com>
2024-08-19 14:50:19 +0300

    ff

--- modul 5/module5hard.py
+++ modul 5/module5hard.py
@@ -16,4 +16,20 @@ class Video:
         self.adult_mode = adult_mode
 
 class UrTube:
+    def __init__(self):
+        self.user = []
+        self.videos = []
+        self.current_user = []
     
+    def log_in(self, nickname,password):
+        
+        self.nickname = nickname
+        self.password = password
+        
+        for i in self.user():
+            if i[0] == self.nickname and i[1] == self.password:
+                self.current_user = self.nickname
+                self.veryfy_user = 1
+            elif i[0] == self.nickname and i[1] != self.password:
+                self.veryfy_user = 2
+        return self.veryfy_user
